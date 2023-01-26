1518f843b60c116ce0cd914c7a47487d715a52bf
nurulhanina <wid190044@siswa.um.edu.my>
2023-01-27 00:45:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import gspread
 import oauth2client
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from PyPDF2 import *
 
 #Define Database
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
@@ -26,7 +27,9 @@ url_image="Resources/Header.png"
 #Declaration of variables
 word_search=""
 classType=["Additive Manufacturing", "Augmented Reality","Autonomous Robots","Big Data","Cloud Computing", "Cyber Physical System","Internet of Service", "Internet of Things","Simulation","Other"]
-sameas=["","","","",""]
+alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+l_alpha=[]
+
 
 #DataFrame
 eng_word=list(df['EN'])
@@ -50,6 +53,17 @@ sameas2=list(df1['sameas3'])
 sameas3=list(df1['sameas4'])
 sameas4=list(df1['sameas5'])
 newword=list(df2['new'])
+
+for i in range(len(alpha)):
+    l=[]
+    for j in eng_word:
+        j=str(j)
+        if j is not None:
+            if j[0].isdigit():
+                continue
+            if j[0].lower()==alpha[i]:
+                l.append(j)
+    l_alpha.append(l)
 
 #functions
 def printtext(text_w):
@@ -134,10 +148,14 @@ def knowexp(cx,text):
     newsame=["","","","",""]
     st.text("\nThe word you selected is: "+text)
     new_defen=st.text_input("Insert the English Definition",key="DEF_EN")
+    st.caption("Make sure that the spelling is correct")
     new_my=st.text_input("Insert the Terminology's Malay Definition",key="MY")
+    st.caption("Make sure that the spelling is correct")
     new_defmy=st.text_input("Insert the Definition of the Terminology's Malay Translation",key="DEF_MY")
+    st.caption("Make sure that the spelling is correct")
     classadd=write_hierarchy()
     newclass=classadd
+    st.caption("Make sure that the spelling is correct")
     newsame[0]=(st.text_input("Insert the Synonym",key="SA1"))
     newsame[1]=(st.text_input("Insert the Synonym",key="SA2"))
     newsame[2]=(st.text_input("Insert the Synonym",key="SA3"))
@@ -151,6 +169,32 @@ def knowexp(cx,text):
         sheet_instance2.append_row(new_row2)
         if cx>0 :
             sheet_instance3.delete_row(cx)
+            
+def gloss_column(ij):
+    l=l_alpha[ij]
+    if l:gloss_err(ij)
+    else:st.write("There is no terminology that starts with this letter")
+          
+def gloss_err(i):
+    l=l_alpha[i]
+    url="https://www.google.com/"  #for testing purpose
+    
+    colms=st.columns((1,2,1))
+    field=["No.","Terminology","Search"]
+    
+    for col, field_Name in zip(colms, field):
+        col.write(field_Name)
+        
+    for x, idx in enumerate(l):
+        col1,col2,col3=st.columns((1,2,1))
+        col1.write(x)
+        col2.write(l[x])
+        button="Search" 
+        button_phold=col3.empty()
+        do_action=button_phold.button(button,key=x+(i*1000))
+        if do_action:
+            searchpage(l[x])
+            button_phold.empty()
 
 def homepage():
     st.title("SISTEM ISTILAH DWIBAHASA")
@@ -241,8 +285,6 @@ def knowledgepage():
         if exist=="":
                 knowexp(0,new_entry)
                                     
-        
-    
 def uploadpage():
     st.title("Sistem Istilah Dwibahasa")
     st.image(url_image)
@@ -251,11 +293,105 @@ def uploadpage():
 def glossarypage():
     st.title("Sistem Istilah Dwibahasa")
     st.image(url_image)
-    st.markdown("Glossary")
+    st.header("Glossary")
+    with st.expander("A Glossary"):
+    st.write("Letter A")
+    gloss_column(0)
+    
+    with st.expander("B Glossary"):
+        st.write("Letter B")
+        gloss_column(1)
+    
+    with st.expander("C Glossary"):
+        st.write("Letter C")
+        gloss_column(2)
+    
+    with st.expander("D Glossary"):
+        gloss_column(3)
+    
+    with st.expander("E Glossary"):
+        gloss_column(4)
+    
+    with st.expander("F Glossary"):
+        gloss_column(5)
+    
+    with st.expander("G Glossary"):
+        gloss_column(6)
+    
+    with st.expander("H Glossary"):
+        gloss_column(7)
+
+    with st.expander("I Glossary"):
+        gloss_column(8)
+    
+    with st.expander("J Glossary"):
+        gloss_column(9)
+    
+    with st.expander("K Glossary"):
+        gloss_column(10)
+    
+    with st.expander("L Glossary"):
+        st.write("Letter L")
+        gloss_column(11)
+    
+    with st.expander("M Glossary"):
+        st.write("Letter M")
+        gloss_column(12)
+    
+    with st.expander("N Glossary"):
+        st.write("Letter N")
+        gloss_column(13)
+    
+    with st.expander("O Glossary"):
+        st.write("Letter O")
+        gloss_column(14)
+    
+    with st.expander("P Glossary"):
+        st.write("Letter P")
+        gloss_column(15)
+    
+    with st.expander("Q Glossary"):
+        st.write("Letter Q")
+        gloss_column(16)
+    
+    with st.expander("R Glossary"):
+        st.write("Letter R")
+        gloss_column(17)
+
+    with st.expander("S Glossary"):
+        st.write("Letter S")
+        gloss_column(18)
+    
+    with st.expander("T Glossary"):
+        st.write("Letter T")
+        gloss_column(19)
+    
+    with st.expander("U Glossary"):
+        st.write("Letter U")
+        gloss_column(20)
+    
+    with st.expander("V Glossary"):
+        st.write("Letter V")
+        gloss_column(21)
+    
+    with st.expander("W Glossary"):
+        st.write("Letter W")
+        gloss_column(22)
+        
+    with st.expander("X Glossary"):
+        st.write("Letter X")
+        gloss_column(23)
+    
+    with st.expander("Y Glossary"):
+        st.write("Letter Y")
+        gloss_column(24)
+    
+    with st.expander("Z Glossary"):
+        st.write("Letter Z")
+        gloss_column(25)
     
 st.sidebar.title('Navigation Pane')
 options = st.sidebar.radio('Select what you want to display:', ['Home', 'Search Page', 'Knowledge Expert Page', 'Upload Page','Glossary Page'])
-
 
 if options=='Home':
     homepage()
